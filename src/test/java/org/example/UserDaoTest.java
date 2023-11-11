0fa851aa2555014ced35f7dceeed2c8e43bede56
webdevyoo <yjk7454@naver.com>
2023-11-11 19:03:43 +0900

    UserDao테스트 클래스 추가 setUp 메소드 구현 / ConnectionManager클래스 추가 getDataSource 히카리CP 설정

---
+++ src/test/java/org/example/UserDaoTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+public class UserDaoTest {
+    @BeforeEach // 테스트 코드 실행하기 앞서 수행
+    void setUp() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("db_schema.sql")); //db-schema.sql 스크립트 파일을 class pass에서 읽어온 뒤 스크립트에 추가
+        DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource()); // populator script 실행
+    }
+}
